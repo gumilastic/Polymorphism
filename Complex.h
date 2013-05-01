f056a1c43dafdf0c2ef3d55cdca152708c09fcf2
gumilastic <darjeeling7@yahoo.com>
2013-05-01 23:01:24 +0300

    First Version

---
+++ Complex.h
@@ -0,0 +1,12 @@
+// Interface file for Complex class
+
+#import <Foundation/Foundation.h>
+
+@interface Complex : NSObject
+
+@property double real, imaginary;
+-(void) print;
+-(void) setReal: (double) a andImaginary: (double) b;
+-(Complex *) add: (Complex *) f;
+
+@end
---
+++ Fraction.h
@@ -0,0 +1,16 @@
+
+#import <Foundation/Foundation.h>
+
+//define the Fraction class
+
+@interface Fraction : NSObject
+
+@property int numerator, denominator;
+
+-(void) print;
+-(void) setTo: (int) n over: (int) d;
+-(double) convertToNum;
+-(Fraction *) add: (Fraction *) f;
+-(void) reduce;
+
+@end
